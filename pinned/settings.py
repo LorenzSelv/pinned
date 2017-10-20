0a5c29fcb9590e2d6a40272cdb0c454df535184a
Lorenzo Selvatici <selvatici.lorenzo@gmail.com>
2017-10-19 21:51:28 -0700

    Added pythonanywhere to allowed hosts

--- pinned/settings.py
+++ pinned/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = os.environ['PINNED_DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', 'pinned.pythonanywhere.com']
 
 # Application definition
 
