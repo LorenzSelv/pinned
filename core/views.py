de8660447c57f44de4916d73136a404216796151
HenryBall <hfball@ucsc.edu>
2017-10-22 14:29:06 -0700

    added upcoming events

--- core/views.py
+++ core/views.py
@@ -1,14 +1,15 @@
 
 from django.shortcuts import render
+from django.views import     generic
+from django.core import      serializers
+from .models import          Event, User
+import                       json
 
-from django.views import generic
-from .models import Event, User
-
-
-class MapView(generic.View):
-
-    def get(self, request, *args, **kwargs):
-        return render(request, 'core/map.html')
+class MapView(generic.ListView):
+    template_name = 'core/map.html'
+    
+    def get_queryset(self):
+        return Event.objects.order_by('-start_date_time').reverse()
 
 
 class EventsView(generic.ListView):
