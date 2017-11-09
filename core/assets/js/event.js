4311b640ac019aef5d889f5257414b2e0560b889
Andrea Silvestroni <96andrea9642@gmail.com>
2017-11-09 04:04:45 -0800

    Show and update participants in events list view

--- core/assets/js/event.js
+++ core/assets/js/event.js
@@ -1,5 +1,5 @@
 // Send ajax request for leaving event, calls successHandler on success and failureHandler on failure
-function sendEventAjax(eventId, action, successHandler, failureHandler){
+function sendEventAjax(eventId, action, successHandler, failureHandler) {
     $.ajax({
         type: "POST",
         url: eventId + action,
@@ -11,64 +11,67 @@ function sendEventAjax(eventId, action, successHandler, failureHandler){
     })
 }
 
-function changeButton(element, action){
-    if(action == 'join'){
+function changeButton(element, action) {
+    if (action == 'join') {
         $(element).removeClass('btn-join').addClass('btn-leave').text("Leave")
     }
 
-    if(action == 'leave'){
+    if (action == 'leave') {
         $(element).removeClass('btn-leave').addClass('btn-join').text("Join")
     }
 
     bindButtons()
 }
 
-function updateParticipants(participants){
+function updateParticipants(element, participants) {
     let tbody = $('.event-participants-list').find('tbody')
-    let amtParticipants = $('.event-participants-amount')
+    let amtParticipants = $(element).parents('.event').find('.event-participants-amount')
 
-    tbody.empty()
-    if(!participants.length)
-        tbody.append("<tr><td>No participant yet</td></tr>")    
-    for (let i = participants.length - 1; i >= 0; i--) {
-        tbody.append("<tr><td>" + participants[i] + "</td></tr>")
+    if (getEventId(element)) {
+        tbody.empty()
+        if (!participants.length)
+            tbody.append("<tr><td>No participant yet</td></tr>")
+        for (let i = participants.length - 1; i >= 0; i--) {
+            tbody.append("<tr><td>" + participants[i] + "</td></tr>")
+        }
+
     }
 
     amtParticipants.text(participants.length)
 }
 
 function joinSuccessHandler(element, data) {
-    if(data.result){
+    if (data.result) {
         changeButton(element, 'join')
-        updateParticipants(data.participants)
+        updateParticipants(element, data.participants)
     }
 }
 
-function leaveSuccessHandler(element, data){
-    if(data.result){
+function leaveSuccessHandler(element, data) {
+    if (data.result) {
         changeButton(element, 'leave')
-        updateParticipants(data.participants)
+        updateParticipants(element, data.participants)
     }
 }
 
-function failureHandler(firstArgument, error){
+function failureHandler(firstArgument, error) {
     console.log(error)
 }
 
 
-function getEventId(element){
+function getEventId(element) {
     let id = $(element).parents(".event").find(".event-id").text()
 
     return id ? id + '/' : ''
 }
 
-function bindButtons(){
+function bindButtons() {
     $(".btn-join").off("click").on("click", function() {
-        sendEventAjax(getEventId($(this)), 'join', (data) => {joinSuccessHandler($(this), JSON.parse(data))}, failureHandler)
+        sendEventAjax(getEventId($(this)), 'join', (data) => { joinSuccessHandler($(this), JSON.parse(data)) }, failureHandler)
     })
 
-    $(".btn-leave").off("click").on("click", function(){
-        sendEventAjax(getEventId($(this)), 'leave', (data) => {leaveSuccessHandler($(this), JSON.parse(data))}, failureHandler)
+    $(".btn-leave").off("click").on("click", function() {
+        sendEventAjax(getEventId($(this)), 'leave', (data) => { leaveSuccessHandler($(this), JSON.parse(data)) }, failureHandler)
     })
 }
 
