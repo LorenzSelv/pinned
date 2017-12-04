817bce9778dc2d516fb9668b80bd7e75c0d5a1dc
Andrea Silvestroni <96andrea9642@gmail.com>
2017-12-03 23:50:09 -0800

    Redirect to event page on notification click

--- core/assets/js/notifications.js
+++ core/assets/js/notifications.js
@@ -3,8 +3,4 @@ $('.notifications.dropdown').find(".dropdown-item").on("click", function() {
     if (!$(this).siblings().length) {
         $(this).parent().append("<center>No notifications</center>")
     }
-
-    // TODO: add ajax call to save reading notification
-
-    $(this).remove()
 })
